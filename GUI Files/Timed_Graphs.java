57bf207de8e0d57d16a618474f563d1808fc123f
Th0github <43998700+Th0github@users.noreply.github.com>
2018-11-28 00:16:26 +0100

    Add files via upload

---
+++ GUI Files/Timed_Graphs.java
@@ -0,0 +1,234 @@
+package sample;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.StrokeLineCap;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
+import javafx.util.Duration;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Timed_Graphs {
+
+    static Scene scene1;
+    static Stage window;
+    static Stage gameOverWindow;
+    static Label layout;
+    static double orgSceneX, orgSceneY;
+    static final Integer starttime = 10; //edit how long the timer is from here.
+    static Integer seconds = starttime;
+
+    //Game over screen method.
+    private static void setGameOver(){
+        gameOverWindow = new Stage();
+        GridPane grid = new GridPane();
+        gameOverWindow.setTitle("Time's up!");
+        Label timeUsed = new Label("Time: ");
+        GridPane.setConstraints(timeUsed,0,0);
+        Label chromaUsed = new Label("Chromatic reached: ");
+        GridPane.setConstraints(chromaUsed, 0,1);
+        grid.getChildren().addAll(timeUsed,chromaUsed);
+        Scene gameOverScene = new Scene(grid);
+        gameOverWindow.setScene(gameOverScene);
+        gameOverWindow.show();
+    }
+
+    //Timer method.
+    private static void doTime() {
+        Timeline time= new Timeline();
+
+
+        KeyFrame frame= new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>(){
+
+            @Override
+            public void handle(ActionEvent event) {
+
+                //@Asem change this to ++ and modify the instance variable to start at zero for your part.  Also change the if condition.
+                seconds--;
+                layout.setText("T: "+seconds.toString());
+                if(seconds<=0){
+                    seconds = starttime;
+                    time.stop();
+                    window.close();
+                    setGameOver();
+                }
+
+            }
+
+
+        });
+
+        time.setCycleCount(Timeline.INDEFINITE);
+        time.getKeyFrames().add(frame);
+        if(time!= null){
+            time.stop();
+        }
+        time.play();
+
+
+    }
+
+    //Mouse click listener.
+    private static EventHandler<MouseEvent> mousePressedEventHandler = (t) ->
+    {
+        orgSceneX = t.getSceneX();
+        orgSceneY = t.getSceneY();
+
+        Circle c = (Circle) (t.getSource());
+        c.toFront();
+    };
+
+    //Mouse drag listener.
+    private static EventHandler<MouseEvent> mouseDraggedEventHandler = (t) ->
+    {
+        double offsetX = t.getSceneX() - orgSceneX;
+        double offsetY = t.getSceneY() - orgSceneY;
+
+        Circle c = (Circle) (t.getSource());
+
+        c.setCenterX(c.getCenterX() + offsetX);
+        c.setCenterY(c.getCenterY() + offsetY);
+
+        orgSceneX = t.getSceneX();
+        orgSceneY = t.getSceneY();
+    };
+
+    //Create GUI object Circle.
+    private static Circle createCircle(double x, double y, double r, Color color)
+    {
+        Circle circle = new Circle(x, y, r, color);
+
+        circle.setCursor(Cursor.CROSSHAIR);
+
+        circle.setOnMousePressed(mousePressedEventHandler);
+        circle.setOnMouseDragged(mouseDraggedEventHandler);
+
+        return circle;
+    }
+
+    //Create GUI object line.
+    private static Line connect(Circle c1, Circle c2)
+    {
+        Line line = new Line();
+
+        line.startXProperty().bind(c1.centerXProperty());
+        line.startYProperty().bind(c1.centerYProperty());
+
+        line.endXProperty().bind(c2.centerXProperty());
+        line.endYProperty().bind(c2.centerYProperty());
+
+        line.setStrokeWidth(1);
+        line.setStrokeLineCap(StrokeLineCap.BUTT);
+        line.getStrokeDashArray().setAll(1.0, 4.0);
+
+        return line;
+    }
+
+    public static void display(String title, String message){
+        window = new Stage();
+
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+        Group gr1 = new Group();
+        scene1 = new Scene(gr1,800, 600);
+
+        Circle Circle1 = createCircle(100, 50, 20, Color.GREY);
+        Circle Circle2 = createCircle(220, 100, 20, Color.GREY);
+        Circle Circle3 = createCircle(440, 350, 20, Color.GREY);
+        Circle Circle4 = createCircle(360, 220, 20, Color.GREY);
+        Circle Circle5 = createCircle(580, 300, 20, Color.GREY);
+        Circle Circle6 = createCircle(290, 500, 20, Color.GREY);
+
+        Line line1 = connect(Circle1, Circle2);
+        Line line2 = connect(Circle1, Circle3);
+        Line line3 = connect(Circle1, Circle4);
+        Line line4 = connect(Circle2, Circle3);
+        Line line5 = connect(Circle4, Circle5);
+        Line line6 = connect(Circle4, Circle6);
+        Line line7 = connect(Circle5, Circle6);
+
+        //add the circles
+        gr1.getChildren().add(Circle1);
+        gr1.getChildren().add(Circle2);
+        gr1.getChildren().add(Circle3);
+        gr1.getChildren().add(Circle4);
+        gr1.getChildren().add(Circle5);
+        gr1.getChildren().add(Circle6);
+
+        // add the lines
+        gr1.getChildren().add(line1);
+        gr1.getChildren().add(line2);
+        gr1.getChildren().add(line3);
+        gr1.getChildren().add(line4);
+        gr1.getChildren().add(line5);
+        gr1.getChildren().add(line6);
+        gr1.getChildren().add(line7);
+
+        //for the timer
+        layout = new Label();
+        layout.setText("T: 10");
+        doTime();
+        gr1.getChildren().addAll(layout);
+
+
+
+        // bring the circles to the front of the lines
+        Circle1.toFront();
+        Circle2.toFront();
+        Circle3.toFront();
+        Circle4.toFront();
+        Circle5.toFront();
+        Circle6.toFront();
+
+
+
+        ColorBox cbox = new ColorBox();
+        //for the part to color the box.
+        //Unfortunately I could not find a better solution than this for now.  Cause I am not 100% how you represent the graphs exactly.
+        ArrayList<Circle> list = new ArrayList<Circle>();
+        list.add(Circle1);
+        list.add(Circle2);
+        list.add(Circle3);
+        list.add(Circle4);
+        list.add(Circle5);
+        list.add(Circle6);
+        Iterator<Circle> iterator = list.iterator();
+
+        while (iterator.hasNext()){
+            Circle temp = iterator.next();
+            temp.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    temp.setFill(cbox.getValue());
+                }
+            });
+        }
+        // Display the ColorBox.
+        cbox.display();
+
+        window.setScene(scene1);
+        window.setTitle("Graph Coloring Game");
+        window.show();
+
+    }
+
+}
