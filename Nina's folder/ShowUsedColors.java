34d254a32744da6347bae99aee7328b8fcca2281
NKrommedijk <43991698+NKrommedijk@users.noreply.github.com>
2018-12-11 13:20:02 +0100

    Add files via upload

---
+++ Nina's folder/ShowUsedColors.java
@@ -0,0 +1,40 @@
+
+
+        package sample;
+
+        import javafx.stage.*;
+        import javafx.scene.*;
+        import javafx.scene.layout.*;
+        import javafx.scene.text.Text;
+
+
+public class ShowUsedColors {
+    public static void display (){
+        Stage window = new Stage();
+        GridPane layout = new GridPane();
+        Scene scene = new Scene(layout, 300, 100);
+        window.setScene(scene);
+        window.setScene(scene);
+        window.setTitle("title");
+        Text text = new Text("You've used " + (Random_Graphs.getNumColors()) + " colors so far.");
+        int x = Backtracking_Alg.getChromNumber();
+        if(x > (Random_Graphs.getNumColors())){
+            Text text2 = new Text("You can use more colours");
+            layout.add(text2, 0, 50, 2, 1);
+        }
+        else if( x == (Random_Graphs.getNumColors())){
+            Text text2 = new Text("You are doing just fine");
+            layout.add(text2, 0, 50, 2, 1);
+
+        }
+        else{
+            Text text2 = new Text("You can use less colours");
+            layout.add(text2, 0, 50, 2, 1);
+
+        }
+
+        layout.add(text, 0, 0, 2, 1);
+
+        window.show();
+    }
+}
