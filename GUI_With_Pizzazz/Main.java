55b084401f9db86ed85f062a6f73d781dee3573d
Th0github <43998700+Th0github@users.noreply.github.com>
2018-12-13 18:20:57 +0100

    Update Main.java

--- GUI_With_Pizzazz/Main.java
+++ GUI_With_Pizzazz/Main.java
@@ -97,13 +97,7 @@ public class Main extends Application {
         layout1.setVgap(10);
         layout1.setAlignment(Pos.TOP_CENTER);
 
-
-        String imagePath = new String("/Users/slav/Documents/JAVA%20projects/Graph_Game/src/sample/oriental.jpg");
-        layout1.setStyle("-fx-background-image: url('file://"+imagePath+"');" +
-                "-fx-background-size: cover");
-
-//        layout1.setStyle("-fx-background-image: url('https://ae01.alicdn.com/kf/HTB11jNebVmWBuNjSspdq6zugXXa2/Home-decoration-art-oriental-girl-flowers-fan-Silk-Fabric-Poster-Print-DM172.jpg_640x640.jpg');" +
-//                "-fx-background-size: cover");
+        layout1.setStyle("-fx-background-image: url('https://ae01.alicdn.com/kf/HTB11jNebVmWBuNjSspdq6zugXXa2/Home-decoration-art-oriental-girl-flowers-fan-Silk-Fabric-Poster-Print-DM172.jpg_640x640.jpg'); -fx-background-size: cover");
 
         //Close window
         window.setOnCloseRequest(e -> {
